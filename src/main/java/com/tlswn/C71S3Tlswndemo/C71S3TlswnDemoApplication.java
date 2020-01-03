289209f5ef0e9943db62a054e40b969204b90381
H P <H P@LAPTOP-9OAR72LE>
2020-01-03 20:02:33 +0800

    项目

---
+++ src/main/java/com/tlswn/C71S3Tlswndemo/C71S3TlswnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tlswn.C71S3Tlswndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C71S3TlswnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C71S3TlswnDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlswn/C71S3Tlswndemo/C71S3TlswnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlswn.C71S3Tlswndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C71S3TlswnDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
