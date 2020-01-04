56b14f5dad647fa2c0faed750dbfa3782d316141
H P <H P@LAPTOP-9OAR72LE>
2020-01-04 19:07:38 +0800

    页面

--- src/main/java/com/tlswn/C71S3Tlswndemo/C71S3TlswnDemoApplication.java
+++ src/main/java/com/tlswn/C71S3Tlswndemo/C71S3TlswnDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*@MapperScan("com.tlswn.C71S3Tlswndemo")*/
 public class C71S3TlswnDemoApplication {
 
 	public static void main(String[] args) {
