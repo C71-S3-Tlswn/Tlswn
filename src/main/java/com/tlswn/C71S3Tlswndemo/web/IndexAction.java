5b46aa6f34c6a053c2d963ca7e3a2b418daaac35
ASUS-PC <ASUS-PC@DESKTOP-7E11BGQ>
2020-02-11 15:21:55 +0800

    查找第一次

--- src/main/java/com/tlswn/C71S3Tlswndemo/web/IndexAction.java
+++ src/main/java/com/tlswn/C71S3Tlswndemo/web/IndexAction.java
@@ -1,13 +1,28 @@
 package com.tlswn.C71S3Tlswndemo.web;
 
+import javax.annotation.Resource;
+import javax.el.VariableMapper;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.tlswn.C71S3Tlswndemo.dao.CommodityMapper;
+import com.tlswn.C71S3Tlswndemo.dao.TypeMapper;
+import com.tlswn.C71S3Tlswndemo.dao.VarietyMapper;
 
 @Controller
 public class IndexAction {
+	@Resource
+	private CommodityMapper cm;
+	@Resource
+	private VarietyMapper vm;
+	@Resource
+	private TypeMapper tm;
 	
 	@GetMapping({"/","index","index.do"})
-	public String Index(){
+	public String Index(Model m){
+		m.addAttribute("nuts", cm.selectByExample(null));
 		return "index";
 	}
 
