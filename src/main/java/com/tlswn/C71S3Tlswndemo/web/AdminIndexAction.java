b36856bfeabe38485a0f71d8154a99412e08411a
song-github1 <1350979635@qq.com>
2020-02-12 21:26:32 +0800

    修改地址冲突问题

--- src/main/java/com/tlswn/C71S3Tlswndemo/web/AdminIndexAction.java
+++ src/main/java/com/tlswn/C71S3Tlswndemo/web/AdminIndexAction.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminIndexAction {
 	
-	@GetMapping({"/","adminindex","adminindex.do"})
+	@GetMapping({"adminindex","adminindex.do"})
 	public String Index(){
 		return "adminindex";
 	}
