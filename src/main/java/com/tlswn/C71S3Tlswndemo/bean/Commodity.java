23d13cd2130759057ec3366cccd8bcb3ac39bd1b
H P <H P@LAPTOP-9OAR72LE>
2020-02-13 19:21:54 +0800

    修复问题

--- src/main/java/com/tlswn/C71S3Tlswndemo/bean/Commodity.java
+++ src/main/java/com/tlswn/C71S3Tlswndemo/bean/Commodity.java
@@ -26,8 +26,20 @@ public class Commodity {
     private Integer tid;
 
     private Integer sid;
+    
+    private Type type;
+    
+    
 
-    public Integer getCid() {
+    public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public Integer getCid() {
         return cid;
     }
 
