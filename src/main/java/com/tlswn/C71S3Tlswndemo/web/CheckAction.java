755bc1cbc3c114445c23a33cbdb8e700ea244fd7
H P <H P@LAPTOP-9OAR72LE>
2020-02-14 19:36:28 +0800

    订单的显示

--- src/main/java/com/tlswn/C71S3Tlswndemo/web/CheckAction.java
+++ src/main/java/com/tlswn/C71S3Tlswndemo/web/CheckAction.java
@@ -1,8 +1,13 @@
 package com.tlswn.C71S3Tlswndemo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.tlswn.C71S3Tlswndemo.bean.User;
+
 
 @Controller
 public class CheckAction {
@@ -11,5 +16,9 @@ public class CheckAction {
 	public String Check(){
 		return "checkout";
 	}
+	/*@ModelAttribute
+	public void init(Model m,@SessionAttribute("User") User user){
+		
+	}*/
 	
 }
