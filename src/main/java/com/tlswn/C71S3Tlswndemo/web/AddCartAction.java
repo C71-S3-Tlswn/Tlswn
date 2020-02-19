b06a3165558ebf6434d755f2f2b07a0435fb27a4
ASUS-PC <ASUS-PC@DESKTOP-7E11BGQ>
2020-02-19 23:52:06 +0800

    购物车方法二

--- src/main/java/com/tlswn/C71S3Tlswndemo/web/AddCartAction.java
+++ src/main/java/com/tlswn/C71S3Tlswndemo/web/AddCartAction.java
@@ -1,5 +1,7 @@
 package com.tlswn.C71S3Tlswndemo.web;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
@@ -10,31 +12,50 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.tlswn.C71S3Tlswndemo.bean.Cart;
+import com.tlswn.C71S3Tlswndemo.bean.CartExample;
 import com.tlswn.C71S3Tlswndemo.bean.User;
 import com.tlswn.C71S3Tlswndemo.biz.CartBiz;
+import com.tlswn.C71S3Tlswndemo.dao.CartMapper;
 import com.tlswn.C71S3Tlswndemo.vo.Result;
 
 @Controller
 public class AddCartAction {
 
 	@Resource
-	private CartBiz cbiz; 
-	
+	private CartBiz cbiz;
+	@Resource
+	private CartMapper cm;
+
 	@PostMapping("/addcart")
 	public Result addcart(Cart cart,
 			@SessionAttribute("User") User user,
 			Error error,Model m){
-		cart.setUid(user.getUid());
-		cbiz.add(cart);
-		m.addAttribute("cart", cart);
-		if(cart.getUid()==null&&cart.getUid().equals("")){
-			return new Result(3, "您尚未登录，请登录");
-		}else if(cart.getCount()==null&&cart.getCount().equals("")){
-			return new Result(2,"添加失败，请选择数量");
-		}else if(cart.getCtemp()==null&&cart.getCtemp().equals("")){
-			return new Result(0, "您没有选择尺码，请选择尺码");
+		CartExample ce=new CartExample();
+		ce.createCriteria().andCidEqualTo(cart.getCid()).andCtempEqualTo(cart.getCtemp()).andUidEqualTo(user.getUid());
+		List<Cart> old= cm.selectByExample(ce);
+		if(old==null || old.isEmpty()){
+			cart.setUid(user.getUid());
+			
+			m.addAttribute("cart", cart);
+			if(cart.getUid()==null&&cart.getUid().equals("")){
+				return new Result(3, "您尚未登录，请登录");
+			}else if(cart.getCount()==null&&cart.getCount().equals("")){
+				return new Result(2,"添加失败，请选择数量");
+			}else if(cart.getCtemp()==null&&cart.getCtemp().equals("")){
+				return new Result(0, "您没有选择尺码，请选择尺码");
+			}else{
+				cbiz.add(cart);
+			return new Result(1, "添加成功", cart);
+			}
+			
 		}else{
-		return new Result(1, "添加成功", cart);
+			CartExample ce2=new CartExample();
+			ce2.createCriteria().andUidEqualTo(user.getUid()).
+			andCtempEqualTo(cart.getCtemp()).andCidEqualTo(cart.getCid());
+			/*cm.updateByExampleNum(count, ce2);*/
+			return new Result(1, "添加成功");
 		}
+			
 	}
+
 }
