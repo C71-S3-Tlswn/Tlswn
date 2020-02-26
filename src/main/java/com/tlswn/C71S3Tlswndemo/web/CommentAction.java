ae1b6de5da72d778f7fe53e1c0196f96c7a7f6c9
song-github1 <1350979635@qq.com>
2020-02-26 22:48:39 +0800

    评论功能再完善

--- src/main/java/com/tlswn/C71S3Tlswndemo/web/CommentAction.java
+++ src/main/java/com/tlswn/C71S3Tlswndemo/web/CommentAction.java
@@ -1,19 +1,20 @@
 package com.tlswn.C71S3Tlswndemo.web;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.tlswn.C71S3Tlswndemo.bean.Commodity;
-import com.tlswn.C71S3Tlswndemo.bean.Evaluate;
-import com.tlswn.C71S3Tlswndemo.bean.EvaluateExample;
-import com.tlswn.C71S3Tlswndemo.bean.Stock;
-import com.tlswn.C71S3Tlswndemo.bean.StockExample;
 import com.tlswn.C71S3Tlswndemo.dao.CommodityMapper;
 @Controller
 public class CommentAction {
@@ -32,5 +33,24 @@ public class CommentAction {
 	    m.addAttribute("commodity", comm);
 		return "comment";
 		
+	}
+	@ResponseBody
+	@PostMapping("upload")
+	public String update(MultipartFile img,@Value("${user.file.path}") String filePath) {
+		String fileName=null;
+		try {
+			// 保存图片
+			/*File file = new File(filePath + img.getOriginalFilename());*/
+			File file=new File(filePath);
+			if(!file.exists()){
+				file.mkdirs();
+			}
+			fileName=img.getOriginalFilename();
+			file=new File(filePath + fileName);
+			img.transferTo(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return fileName;
 	}
 }
