26945cf9460515b94c10ac2ce8e36c0c91708e00
song-github1 <1350979635@qq.com>
2020-03-12 20:20:33 +0800

    完善

--- src/main/java/com/tlswn/C71S3Tlswndemo/C71S3TlswnDemoApplication.java
+++ src/main/java/com/tlswn/C71S3Tlswndemo/C71S3TlswnDemoApplication.java
@@ -3,12 +3,14 @@ package com.tlswn.C71S3Tlswndemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 
 @SpringBootApplication
 @MapperScan("com.tlswn.C71S3Tlswndemo")
+@ServletComponentScan(basePackages = "com.tlswn.C71S3Tlswndemo")
 public class C71S3TlswnDemoApplication {
 
 	public static void main(String[] args) {
